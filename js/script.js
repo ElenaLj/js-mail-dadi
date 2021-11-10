31a08a2c6a34deef64f0a26a309689cfa404af44
ElenaLj <elenaljubijankic@gmail.com>
2021-11-10 14:36:19 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// alert("Hello World!");
+
+//esercizio 1: MAIL 
+    //  1) Chiedi all’utente la sua email
+    //  2) Controlla che sia nella lista di chi può accedere
+    //  3) Stampa un messaggio appropriato sull’esito del controllo.
+    
